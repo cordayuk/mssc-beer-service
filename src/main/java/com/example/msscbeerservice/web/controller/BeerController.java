5d53fcef99b51bb31f57d9d176f696dc8b21001c
Chris Corday <corday@gmail.com>
2021-02-14 11:54:28 +0000

    Fixed Api mapping

--- src/main/java/com/example/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/example/msscbeerservice/web/controller/BeerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/beer")
+@RequestMapping("/api/v1/beer/")
 @RestController
 public class BeerController {
 
