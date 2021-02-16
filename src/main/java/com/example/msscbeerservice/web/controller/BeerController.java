c702b818eea52f0e15e45bdb31a65ba57d029d51
Chris Corday <corday@gmail.com>
2021-02-16 19:24:26 +0000

    Added annotation to controller

--- src/main/java/com/example/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/example/msscbeerservice/web/controller/BeerController.java
@@ -4,6 +4,7 @@ import com.example.msscbeerservice.web.model.BeerDto;
 import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,14 +25,14 @@ public class BeerController {
   }
 
   @PostMapping
-  public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto)
+  public ResponseEntity saveNewBeer(@Validated @RequestBody BeerDto beerDto)
   {
     // TODO: implement
     return new ResponseEntity(HttpStatus.CREATED);
   }
 
   @PutMapping("/{beerId}")
-  public ResponseEntity updateBeerById( @PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto)
+  public ResponseEntity updateBeerById( @PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto)
   {
     //TODO: implement
     return new ResponseEntity(HttpStatus.NO_CONTENT);
